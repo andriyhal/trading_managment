1b486a54e77d7395e76fb1db324598073c0e167f
Anton <mamant4444@gmail.com>
2023-02-07 15:51:55 +0200

    testing branch

--- components/bot.js
+++ components/bot.js
@@ -1,6 +1,62 @@
 import React, {useCallback, useEffect, useState} from "react";
-import {Checkbox, FormControl, FormGroup, Input} from "@mui/material";
-import {styles} from "../styles/styles";
+import {Box, Checkbox, FormControl, FormGroup, Input, Typography} from "@mui/material";
+
+const styles = {
+    form: {
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    containerCheckbox: {
+        width: 300,
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center'
+    },
+    checkboxText: {
+        fontWeight: 700,
+        fontSize: 16
+    },
+    label: {
+        width: 270,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        textAlign: 'center',
+        marginBottom: 10
+    },
+    input: {
+        display: 'block',
+        width: 270,
+        paddingLeft: 10,
+        boxSizing: 'border-box',
+        fontSize: 22,
+        border: '1px solid #e0e0e0',
+        outline: 'none',
+        borderRadius: 4
+    },
+    typography: {
+        width: 100,
+        textAlign: 'center',
+        fontSize: 16,
+        fontWeight: 700,
+        color: '#000'
+    },
+    buttonBot: {
+        minWidth: 250,
+        height: 40,
+        marginTop: 15,
+        fontWeight: 700,
+        fontSize: 20,
+        color: '#fff',
+        backgroundColor: '#367010',
+        border: '2px solid #367010',
+        borderRadius: 5,
+        cursor: "pointer"
+    }
+}
 
 const BotForm = ({fetcher, defaultBotInfo = null}) => {
     const [botInfo, setBotInfo] = useState({});
@@ -12,7 +68,7 @@ const BotForm = ({fetcher, defaultBotInfo = null}) => {
     }, [botInfo])
 
     useEffect(() => {
-        if(defaultBotInfo){
+        if (defaultBotInfo) {
             setBotInfo(defaultBotInfo)
         }
     }, [defaultBotInfo])
@@ -26,18 +82,36 @@ const BotForm = ({fetcher, defaultBotInfo = null}) => {
     }, [botInfo])
 
     return (
-        <FormGroup method='post'>
+        <FormGroup style={styles.form} method='post'>
             {console.log({botInfo})}
-            <Checkbox onChange={onBotInfoChange} defaultChecked={botInfo.isSellFirst} name='isSellFirst' placeholder='Sell first' id='isSellFirst'/>
-            <Checkbox onChange={onBotInfoChange}
-                      defaultChecked={botInfo.isActive} name='isActive' placeholder='Active'/>
-            <Input onChange={onBotInfoChange}
-                   type='number' value={botInfo.buyPrice} name='buyPrice' placeholder='Buy price'/>
-            <Input onChange={onBotInfoChange}
-                   type='number' value={botInfo.sellPrice} name='sellPrice' placeholder='Sell price'/>
-            <Input onChange={onBotInfoChange}
-                   type='text'  name='buyQuantity' value={botInfo.buyQuantity} placeholder='Buy quantity' id="buyQuantity"/>
-            <button style={styles.smallButton} type='submit' onClick={onSaveBotForm}>{defaultBotInfo ? "Update bot" : "Create bot"}</button>
+            <Box style={styles.containerCheckbox}>
+                <Checkbox onChange={onBotInfoChange} defaultChecked={botInfo.isSellFirst} name='isSellFirst'
+                          placeholder='Sell first' id='isSellFirst'/>
+                <Typography style={styles.checkboxText}>isSellFirst</Typography>
+            </Box>
+            <Box style={styles.containerCheckbox}>
+                <Checkbox onChange={onBotInfoChange}
+                          defaultChecked={botInfo.isActive} name='isActive' placeholder='Active'/>
+                <Typography style={styles.checkboxText}>isActive</Typography>
+            </Box>
+            <label style={styles.label}>
+                <Typography style={styles.typography}>Buy price</Typography>
+                <Input style={styles.input} onChange={onBotInfoChange} type='number'
+                       value={botInfo.buyPrice} name='buyPrice' placeholder='Buy price'/>
+            </label>
+            <label style={styles.label}>
+                <Typography style={styles.typography}>Sell price</Typography>
+                <Input style={styles.input} onChange={onBotInfoChange} type='number'
+                       value={botInfo.sellPrice} name='sellPrice' placeholder='Sell price'/>
+            </label>
+            <label style={styles.label}>
+                <Typography style={styles.typography}>Buy quantity</Typography>
+                <Input style={styles.input} onChange={onBotInfoChange} type='text'
+                       value={botInfo.buyQuantity} name='buyQuantity'
+                       placeholder='Buy quantity' id="buyQuantity"/>
+            </label>
+            <button style={styles.buttonBot} type='submit'
+                    onClick={onSaveBotForm}>{defaultBotInfo ? "Update bot" : "Create bot"}</button>
         </FormGroup>
     )
 }
--- components/profiles_layout.js
+++ components/profiles_layout.js
@@ -1,16 +1,53 @@
 import {memo, useCallback, useEffect, useMemo, useState} from "react";
 import axios from "axios";
 import {Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typography} from "@mui/material";
-import { useRouter } from 'next/router'
-import {styles} from "../styles/styles";
+import {useRouter} from 'next/router'
 import Check from "@mui/icons-material/Check";
 
-const profilesLayoutStyles = {
-    display: "flex",
-    flex: 1
+const styles = {
+    wrapperProfiles: {
+        display: "flex",
+        marginLeft: 10
+    },
+    buttonProfiles: {
+        minWidth: 24,
+        fontWeight: 700,
+        marginLeft: 10,
+        fontSize: 18,
+        color: '#fff',
+        backgroundColor: '#000',
+        border: '2px solid #000',
+        borderRadius: 5,
+        cursor: "pointer"
+    },
+    blockProfiles: {
+        minWidth: 230,
+        minHeight: 500,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'left',
+        alignItems: 'center',
+        border: '2px solid #000',
+        borderRadius: 5,
+        marginTop: 10,
+        marginLeft: 5
+    },
+    titleProfiles: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '100%',
+        height: 50,
+        fontSize: 24,
+        fontWeight: 700,
+        borderBottom: '2px solid #000'
+    },
+    profileRow: {
+        height: 20
+    }
 }
 
-const ProfileRow = memo(({ id, name }) => {
+const ProfileRow = memo(({id, name}) => {
     const router = useRouter()
 
     const onSelectProfile = useCallback(() => {
@@ -21,9 +58,9 @@ const ProfileRow = memo(({ id, name }) => {
         <ListItem component="div" disablePadding>
             <ListItemButton onClick={onSelectProfile}>
                 {id === router.query.profileId ? <ListItemIcon>
-                    <Check />
+                    <Check/>
                 </ListItemIcon> : null}
-                <ListItemText primary={name} />
+                <ListItemText primary={name}/>
             </ListItemButton>
         </ListItem>
     )
@@ -45,24 +82,20 @@ const ProfilesLayout = ({children = null}) => {
     }, [])
 
     const profilesRender = useMemo(() => {
-        return <>{profiles.map( profileInfo => <ProfileRow {...profileInfo} key={profileInfo.id}/>)}</>
+        return <> {profiles.map(profileInfo =>
+            <ProfileRow {...profileInfo} key={profileInfo.id}/>)}</>
     }, [profiles])
 
     return (
-        <Box sx={profilesLayoutStyles}>
-            <Box sx={{ minWidth: 250}}>
-                <Typography variant="h5" gutterBottom>
+        <Box style={styles.wrapperProfiles}>
+            <Box style={styles.blockProfiles}>
+                <Typography style={styles.titleProfiles}>
                     Profiles
-                    <button style={styles.smallButton} onClick={onCreateProfileLinkClick}>+</button>
+                    <button style={styles.buttonProfiles} onClick={onCreateProfileLinkClick}>+</button>
                 </Typography>
-                {profiles.length ?  <List
-                    sx={{ width: '100%', bgcolor: 'background.paper' }}
-                >
-                    {profilesRender}
-                </List> : null}
-                {!profiles.length ? <>{"Have not profiles yet"}</> : null}
+                {profiles.length ? <List> {profilesRender} </List> : null}
+                {!profiles.length ? <> {"Have not profiles yet"} </> : null}
             </Box>
-
             {children && children}
         </Box>
     );
